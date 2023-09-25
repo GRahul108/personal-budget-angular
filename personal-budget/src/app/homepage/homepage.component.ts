2a9d449e13e33beea85daf4aecd7334d16a29124
GRahul108 <rahulchand810@gmail.com>
2023-09-24 23:51:08 -0400

    Added dataService chartjs d3js

--- personal-budget/src/app/homepage/homepage.component.ts
+++ personal-budget/src/app/homepage/homepage.component.ts
@@ -1,6 +1,8 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit} from '@angular/core';
 import {  HttpClient, HttpClientModule } from '@angular/common/http';
 import { Chart } from 'chart.js/auto';
+import * as d3 from 'd3';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'pb-homepage',
@@ -8,40 +10,27 @@ import { Chart } from 'chart.js/auto';
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements AfterViewInit {
-  public dataSource:any = {
-    datasets: [
-        {
-            data: [],
-            backgroundColor: [
-                '#ffcd56',
-                '#ff6384',
-                '#36a2eb',
-                '#fd6b19',
-                '#eb87f1',
-                '#87f195',
-                '#405e44'
-            ]
-        }
-    ],
-    labels: []
-};
-  constructor ( private http:HttpClient ){
+
+  private svg: any;
+  private margin = 50;
+  private width = 750;
+  private height = 600;
+  // The radius of the pie chart is half the smallest side
+  private radius = Math.min(this.width, this.height) / 2 - this.margin;
+  private colors:any;
+
+
+  constructor ( private http:HttpClient,private dataService: DataService ){
 
   }
   ngAfterViewInit(): void {
-    this.http.get('http://localhost:3002/budget')
-    .subscribe((res:any) => {
-      console.log(res)
-      for (var i = 0; i < res.myBudget.length; i++) {
-        this.dataSource.datasets[0].data[i] = res.myBudget[i].budget;
-        this.dataSource.labels[i] = res.myBudget[i].title;
-
-
-    }
+    this.dataService.fetchData().subscribe(()=>{
 
     this.createChart();
-
-    })
+    this.createSvg();
+  this.createColors();
+  this.drawChart();
+  })
 
   }
 
@@ -53,11 +42,67 @@ export class HomepageComponent implements AfterViewInit {
     var myPieChart= new Chart(ctx,
       {
         type:'pie',
-        data:this.dataSource
+        data:this.dataService.dataSource
       });
 
   }
-
+  createSvg(): void {
+    this.svg = d3.select("#pie")
+    .append("svg")
+    .attr("width", this.width)
+    .attr("height", this.height)
+    .append("g")
+    .attr(
+      "transform",
+      "translate(" + this.width / 2 + "," + this.height / 2 + ")"
+    );
 }
 
+private createColors(): void {
+  this.colors = d3.scaleOrdinal()
+  .domain([...this.dataService.dataSource.labels])
+  .range([...this.dataService.dataSource.datasets[0].backgroundColor]);
 
+}
+private drawChart(): void {
+  // Compute the position of each group on the pie:
+  const pie = d3.pie<any>().value((d: any) =>
+    Number(d)
+  );
+
+  // Build the pie chart
+  const pieData = pie(this.dataService.dataSource.datasets[0].data);
+
+  // Build the pie chart
+  this.svg
+  .selectAll('pieces')
+  .data(pieData)
+  .enter()
+  .append('path')
+  .attr('d', d3.arc()
+    .innerRadius(0)
+    .outerRadius(this.radius)
+  )
+  .attr('fill', (d: any, i: any) => this.colors(this.dataService.dataSource.labels[i]))
+  // .attr('fill', (d: any, i: any) => (this.colors(i)))
+  .attr("stroke", "#121926")
+  .style("stroke-width", "1px");
+
+  // Add labels
+  const labelLocation = d3.arc()
+  .innerRadius(100)
+  .outerRadius(this.radius);
+
+  this.svg
+  .selectAll('pieces')
+  .data(pieData)
+  .enter()
+  .append('text')
+  // .text((d: any)=> d.data.Framework)
+  .text((d: any, i: any) => this.dataService.dataSource.labels[i])
+  .attr("transform", (d: any) => "translate(" + labelLocation.centroid(d) + ")")
+  // .attr("transform", (d: any) => "translate(" + labelLocation.centroid(d) + ")")
+  .style("text-anchor", "middle")
+  .style("font-size", 15);
+}
+}
